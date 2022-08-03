493673f96c80c51fc54ff431370e1d9a5b086dfa
“ZJaibat” <jaibatzak@gmail.com>
2022-08-03 16:23:41 -0400

    Trending Stock Complete

--- TrendingStock.py
+++ TrendingStock.py
@@ -2,21 +2,11 @@ from selenium import webdriver
 import pandas as pd
 import time
 
-Tickers = []
-Names = []
-Prices = []
-Change = []
-mktcap = []
-
-
-
 # Defines the website and gives file path to ChromeDriver.
 website = 'https://finance.yahoo.com/trending-tickers'
 PATH  = '/Users/zakariah/Documents/Code/chromedriver'
 driver = webdriver.Chrome(PATH)
 driver.get(website)
-
-
 
 # Locating the table storing it in variable called stocks.
 tickers = driver.find_elements_by_xpath('//tbody/tr/td[1]/a')
@@ -28,27 +18,20 @@ mkt_cap = driver.find_elements_by_xpath('//table/tbody/tr/td[8]')
 
 #converts all the inforamtion to a text string to be displayed
 Stock_result = []
-
 for i in range(len(prices)):
     temporary_data={'Ticker': tickers[i].text,
                    'Name': name[i].text,
                    'Price': prices[i].text,
                    'Change': change[i].text,
-                   'mktcap': mkt_cap[i].text}
+                   'Market_Cap': mkt_cap[i].text}
     Stock_result.append(temporary_data)
-
-
-#Trending_Stock=[]
-#Trending_Stock.append(tempdata)
-
-
 
 # adds the cleaned data to a CSV and exports it.
 df_data = pd.DataFrame(Stock_result)
 df_data.to_csv('Stocks.csv', index=False)
 
-
-
+# Displays message to the user about where more information can be found.
+# Displays the top 5 trending stocks on yahoo trending lists
 print("Todays top 5 trending stocks are: \n")
 print(df_data.head(5))
 print("\n NOTE: A more extensive list is located in the same folder as this program. :)")
