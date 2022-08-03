5a8338ea23aa7a2b541d99f4ae5e80ef07fe44d0
“ZJaibat” <jaibatzak@gmail.com>
2022-08-03 16:19:48 -0400

    All stocks are being displayed correctly, just need to do some cleaning up

--- TrendingStock.py
+++ TrendingStock.py
@@ -1,8 +1,14 @@
-from distutils.util import change_root
-from requests import RequestException
 from selenium import webdriver
 import pandas as pd
 import time
+
+Tickers = []
+Names = []
+Prices = []
+Change = []
+mktcap = []
+
+
 
 # Defines the website and gives file path to ChromeDriver.
 website = 'https://finance.yahoo.com/trending-tickers'
@@ -10,44 +16,43 @@ PATH  = '/Users/zakariah/Documents/Code/chromedriver'
 driver = webdriver.Chrome(PATH)
 driver.get(website)
 
+
+
 # Locating the table storing it in variable called stocks.
-stocks = driver.find_elements_by_tag_name('tr')
+tickers = driver.find_elements_by_xpath('//tbody/tr/td[1]/a')
+name = driver.find_elements_by_xpath('//table/tbody/tr/td[2]')
+prices = driver.find_elements_by_xpath('//tbody/tr/td[3]')
+change = driver.find_elements_by_xpath('//table/tbody/tr/td[6]')
+mkt_cap = driver.find_elements_by_xpath('//table/tbody/tr/td[8]')
 
-# init vairabels "dirty" for the dirty data. Others for labeling purposes.
-dirty = []
-lprice = []
-symbol = []
-name = []
-pchange = []
-mkt_cap = []
 
-# adds the data from website to a local list.
-for stock in stocks:
-    dirty.append(stock.text)
+#converts all the inforamtion to a text string to be displayed
+Stock_result = []
 
-# cleans the data and adds it to the correct list.
-for i in range(1, len(dirty)):
-    dirty[i] = dirty[i].replace('\n', ' ').replace('"', "").split()
-    for number in range(0, len(dirty[i])):
-        if number == 0:
-            symbol.append(dirty[i][number])
-        if number == 1:
-            name.append(dirty[i][number])
-        if number == 4:
-            lprice.append(dirty[i][number])
-        if number == 5:
-            pchange.append(dirty[i][number])
-        if number == 8:
-            mkt_cap.append(dirty[i][number])
+for i in range(len(prices)):
+    temporary_data={'Ticker': tickers[i].text,
+                   'Name': name[i].text,
+                   'Price': prices[i].text,
+                   'Change': change[i].text,
+                   'mktcap': mkt_cap[i].text}
+    Stock_result.append(temporary_data)
+
+
+#Trending_Stock=[]
+#Trending_Stock.append(tempdata)
+
 
 
 # adds the cleaned data to a CSV and exports it.
-df = pd.DataFrame({'TICKER' : symbol, 'NAME' : name, 'PRICE' : lprice, 'CHANGED' : pchange, 'MKTCAP': mkt_cap})
-df.to_csv('Stocks.csv', index=False)
+df_data = pd.DataFrame(Stock_result)
+df_data.to_csv('Stocks.csv', index=False)
+
+
 
 print("Todays top 5 trending stocks are: \n")
-print(df.head(5))
+print(df_data.head(5))
 print("\n NOTE: A more extensive list is located in the same folder as this program. :)")
+
 
 # tells the program to wait before closing.
 time.sleep(15)
